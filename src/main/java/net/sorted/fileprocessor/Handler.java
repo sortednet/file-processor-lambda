9eda162a16beb66b3de9be73b4cffc1ec4456808
dug <dug@sorted.net>
2024-01-10 10:23:30 +1100

    WIP

--- src/main/java/net/sorted/fileprocessor/Handler.java
+++ src/main/java/net/sorted/fileprocessor/Handler.java
@@ -1,106 +1,34 @@
-package net.sorted.fileextractor;
+package net.sorted.fileprocessor;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.S3Event;
-import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification.S3EventNotificationRecord;
-import com.google.gson.Gson;
-import software.amazon.awssdk.core.ResponseBytes;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.GetObjectRequest;
-import software.amazon.awssdk.services.s3.model.GetObjectResponse;
-import software.amazon.awssdk.services.sqs.SqsClient;
-import software.amazon.awssdk.services.sqs.model.GetQueueUrlRequest;
-import software.amazon.awssdk.services.sqs.model.MessageAttributeValue;
-import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
-
-import java.util.Map;
-import java.util.stream.Collectors;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
 
 
-public class Handler implements RequestHandler<S3Event, String>{
-
-    private final String envQueueName = System.getenv("queuename");
-    private final String queueName = (envQueueName != null) ? envQueueName : "file-content-queue";
-
-    private final S3Client s3 = S3Client.builder().region(Region.AP_SOUTHEAST_2).build();
-    private final SqsClient sqsClient = SqsClient.builder().region(Region.AP_SOUTHEAST_2).build();
-
-    private final GetQueueUrlRequest getQueueRequest = GetQueueUrlRequest.builder().queueName(queueName).build();
-    private final String queueUrl = sqsClient.getQueueUrl(getQueueRequest).queueUrl();
-
-    private final Gson gson = new Gson();
+public class Handler implements RequestHandler<SQSEvent, String>{
 
     @Override
-    public String handleRequest(S3Event event, Context context)
+    public String handleRequest(SQSEvent event, Context context)
     {
         LambdaLogger logger = context.getLogger();
-        S3EventNotificationRecord record = event.getRecords().get(0);
-        String srcBucket = record.getS3().getBucket().getName();
-        // Object key may have spaces or unicode non-ASCII characters.
-        String srcKey = record.getS3().getObject().getUrlDecodedKey();
-        logger.log("File Extractor 0.0.14\n");
-        logger.log("RECORD: " + record + "\n");
-        logger.log("SOURCE BUCKET: " + srcBucket + "\n");
-        logger.log("SOURCE KEY: " + srcKey + "\n");
-
+        logger.log("File Processor 0.0.4\n");
         logger.log("EVENT TYPE: " + event.getClass() + "\n");
+        logger.log("EVENT: " + event.toString() + "\n");
+        logger.log("NumRecords: " + event.getRecords().size() + "\n");
 
-        try {
-            String content = processFile(srcBucket, srcKey);
-            logger.log("Bytes read: " + content.length() + "\n");
-            logger.log("READ FILE: " + content);
-        } catch (Throwable e) {
-            logger.log("Error " + e.getMessage() + " \n");
-        }
+        var all = event.getRecords();
+        all.stream().forEach(msg -> logger.log(toMessageLogFormat(msg)));
 
-
-        return srcBucket + "/" + srcKey;
+        return ""+all.size();
     }
 
-    private String processFile(String bucketName, String keyName) throws Exception {
-
-        GetObjectRequest objectRequest = GetObjectRequest
-                .builder()
-                .key(keyName)
-                .bucket(bucketName)
-                .build();
-
-        ResponseBytes<GetObjectResponse> objectBytes = s3.getObjectAsBytes(objectRequest);
-        String data = objectBytes.asUtf8String();
-
-        var messages = data.lines().map(line -> lineToJson(keyName, line)).collect(Collectors.toList());
-        messages.stream().forEach(message -> sendMessage(message));
-
-        return data;
-
-    }
-
-    private void sendMessage(String message) {
-
-        var msg = gson.fromJson(message, Map.class);
-
-        var contentValue = MessageAttributeValue.builder()
-                .stringValue(msg.get("type").toString())
-                .dataType("String")
-                .build();
-        SendMessageRequest sendMsgRequest = SendMessageRequest.builder()
-                .queueUrl(queueUrl)
-                .messageBody(message)
-                .messageAttributes(Map.of("contentType", contentValue))
-                .build();
-
-        sqsClient.sendMessage(sendMsgRequest);
-    }
-
-    private String lineToJson(String file, String line) {
-        String contentType = "X";
-        if (line.contains(",")) {
-            contentType = line.split("[,]")[0];
-        }
-        return gson.toJson(Map.of("content", line, "filename", file, "type", contentType));
+    private String toMessageLogFormat(SQSEvent.SQSMessage message) {
+        SQSEvent.MessageAttribute typeAttr = message.getMessageAttributes().get("contentType");
+        String type = (typeAttr != null) ? typeAttr.getStringValue() : "none" ;
+        return String.format("Type: %s, Content %s",
+                type,
+                message.getBody());
     }
 
 }
